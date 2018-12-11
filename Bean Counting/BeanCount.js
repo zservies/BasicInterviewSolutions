2ff2507616d1aa4a2f0bce0b76c20479e03abf46
Zachary Servies <zachservies@gmail.com>
2018-12-11 16:38:17 -0500

    Add files via upload
    Counts specific characters in a a string.

---
+++ Bean Counting/BeanCount.js
@@ -0,0 +1,28 @@
+//Case sensitive
+function countBs(someString){
+    let count = 0;
+    for(let i = 0; i <= someString.length; i++){
+        if (someString[i] === "B"){
+            count+=1;
+        }else{
+            continue;
+        }
+    }
+    return count;
+}
+
+function countChar(someString, countCharacter){
+    let count = 0;
+    for(let i = 0; i<= someString.length; i++){
+        if (someString[i] === countCharacter){
+            count+=1;
+        }else{
+            continue;
+        }
+    }
+    return count;
+
+}
+
+console.log(countBs("BBC"));
+console.log(countChar("hello this is a string.", "p"));
